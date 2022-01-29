a2620321338935e0afb23e5f9f7efdf06bd07b01
tinykitelab <dakota@tinykitelab.com>
2022-01-29 12:39:30 -0600

    feat: Update moon phase handlers

--- server/handler.js
+++ server/handler.js
@@ -45,46 +45,25 @@ const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res, next) => {
-  return res
-    .status(200)
-    .json({
-      message: "Hello from root!",
-    })
-    .header("Access-Control-Allow-Origin", "*")
-    .header("Access-Control-Allow-Credentials", true);
-  // .header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept"
-  // );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
 });
 
-// app.use(middleware function for cors)
-// Use this for all endpoint responses
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
 
 app.get("/phase", (req, res, next) => {
-  return res
-    .status(200)
-    .json({
-      phase: moonPhase,
-    })
-    .header("Access-Control-Allow-Origin", "*")
-    .header("Access-Control-Allow-Credentials", true);
-  // .header("Access-Control-Allow-Origin", "*")
-  // .header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept"
-  // );
+  return res.status(200).json({
+    phase: moonPhase,
+  });
 });
 
 app.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept"
-  // );
-  // next();
-
   return res.status(404).json({
     error: "Not Found",
   });
